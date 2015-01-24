c15111c74bfcf0420053320af0e04b9ba810635b
okvic77 <okvic77@me.com>
2015-01-24 02:10:46 +0000

    we-configured-the-controllers

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var myFirebaseRef = new Firebase("https://steakim.firebaseio.com/");
 (function($) {
 	
-	var app = angular.module('SteakIm', ['ui.router']);
+	var app = angular.module('SteakIm', ['ui.router', 'firebase']);
 	
 	
 	app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
@@ -11,33 +11,48 @@ var myFirebaseRef = new Firebase("https://steakim.firebaseio.com/");
   //
   // Now set up the states
   $stateProvider
-    .state('state1', {
-      url: "/state1",
-      templateUrl: "partials/state1.html"
+    .state('inicio', {
+      url: "/",
+      templateUrl: "/partials/home.html",
+      controller: ['$scope', function($scope){
+      	
+      }]
     })
-    .state('state1.list', {
-      url: "/list",
-      templateUrl: "partials/state1.list.html",
-      controller: function($scope) {
-        $scope.items = ["A", "List", "Of", "Items"];
+    .state('chat', {
+      url: "/:chat",
+      templateUrl: "/partials/chat.html",
+      controller: ['$scope', '$stateParams', function($scope, $stateParams) {
+		
+		
+		$scope.enviar = function(){
+			console.log('Envio');
+		}
+		
+		$scope.list = [1, 2, 3, 4];
+		
+      	$scope.alias = $stateParams.chat;
+      	
       }
-    })
-    .state('state2', {
-      url: "/state2",
-      templateUrl: "partials/state2.html"
-    })
-    .state('state2.list', {
-      url: "/list",
-      templateUrl: "partials/state2.list.html",
-      controller: function($scope) {
-        $scope.things = ["A", "Set", "Of", "Things"];
+      ],
+      resolve: {
+      	chatFirebase: ['$firebase', '$stateParams', function($firebase, $stateParams){
+      		var chat = $stateParams.chat;
+      		console.log(chat);
+      		//var ref = myFirebaseRef.child();
+      		return 'ok';
+      		//console.log($stateParams);
+      	}]
       }
     });
 }]);
 	
+	
 	// Firebase
 	
-	myFirebaseRef.child("title").on("value", function(snapshot) {
+	/*myFirebaseRef.child("title").on("value", function(snapshot) {
+		$('#live').text(snapshot.val());
+	});*/
+	myFirebaseRef.child("location/state").on("value", function(snapshot) {
 		$('#live').text(snapshot.val());
 	});
 	//console.log("test");
