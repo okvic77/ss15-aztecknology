1420f6d4fac321018b66e044d4fcc8a5e02fdc63
okvic77 <okvic77@me.com>
2015-01-24 20:47:31 +0000

    public

--- js/app.js
+++ js/app.js
@@ -215,17 +215,11 @@ console.log($element);
 
 					//var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
 
-
-	$scope.tipos = [
-    { name: 'Adam',      email: 'adam@email.com',      age: 10 },
-    { name: 'Amalie',    email: 'amalie@email.com',    age: 12 },
-    { name: 'Wladimir',  email: 'wladimir@email.com',  age: 30 },
-    { name: 'Samantha',  email: 'samantha@email.com',  age: 31 },
-    { name: 'Estefanía', email: 'estefanía@email.com', age: 16 },
-    { name: 'Natasha',   email: 'natasha@email.com',   age: 54 },
-    { name: 'Nicole',    email: 'nicole@email.com',    age: 43 },
-    { name: 'Adrian',    email: 'adrian@email.com',    age: 21 }
-];;
+$scope.tmp = {};
+				$scope.guardarPin = function(pin){
+					pin.tipo = $scope.tmp.tipo;
+					$scope.pins.$save(pin);
+				}
 
 					// var EventHandler = $famous['famous/core/EventHandler'];
 					// $scope.eventHandler = new EventHandler();
